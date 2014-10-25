34540c26a92b2b6996f0acd7d1a3858523375864
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-25 13:40:22 +0000

    Added Info
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-landing-page/tags/1.1@1013802 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Ultimate_Landing_Pages.php
+++ Ultimate_Landing_Pages.php
@@ -2,11 +2,17 @@
 /*
 Plugin Name: Ultimate Landing Page Free
 Description: Create Beautiful Landing Pages in minutes
-Author: umarbajwa
+Author: Umar Bajwa
 Plugin URI: http://web-settler.com/ultimate-landing-page/
 Author URI: http://web-settler.com/contact/
 Version: 1.1
 Donate link: http://web-settler.com/ultimate-landing-page/ 
+
+*************
+* Copyright: 2014 http://web-settler.com/
+* Note : License Free to use, Free to modify, Permission required to redistribute.
+* Help : Email : Umar@web-setter.com
+*************
 */
 include 'lpp_settings.php';
 include 'lpp_cs_post_type.php';
