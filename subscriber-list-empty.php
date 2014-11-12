e3f15d2b0197fb639d52c5b32e9c3be08193b378
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-12 11:51:56 +0000

    Added Subscribe List  empty functionality.
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-landing-page/tags/1.1@1024397 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ subscriber-list-empty.php
@@ -0,0 +1,16 @@
+<?php
+
+$f = @fopen("lpp_subscribers_list.csv", "r+");
+if ($f !== false) {
+    ftruncate($f, 0);
+    fclose($f);
+
+    echo 'Success!';
+}
+
+else{
+	echo "Some Error Occurred!";
+}
+
+
+?>
