426c613d76d3b44ac1920669aac5b30f7c3c1063
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-16 13:55:05 +0000

    Few Changes
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-landing-page/trunk@1045714 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,6 @@
 <?php
     //API Key - see http://admin.mailchimp.com/account/api
-    //$apikey = '12312312';
+    //$apikey = 'Your Api Key';
     
     
     
@@ -8,14 +8,13 @@
       
     // A List Id to run examples against. use lists() to view all
     // Also, login to MC account, go to List, then List Tools, and look for the List ID entry
-    //$listId = '31231312';
+    //$listId = 'Your MailChimp List ID';
     
     // A Campaign Id to run examples against. use campaigns() to view all
     $campaignId = 'YOUR MAILCHIMP CAMPAIGN ID - see campaigns() method';
 
     //some email addresses used in the examples:
-    $my_email = 'umarbajwa@ymail.com';
-    $boss_man_email = 'INVALID@example.com';
+    $boss_man_email = 'example@example.com';
 
     //just used in xml-rpc examples
     $apiUrl = 'http://api.mailchimp.com/1.3/';
