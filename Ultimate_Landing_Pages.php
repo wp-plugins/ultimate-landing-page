065be3357ddd0426ab2751fbc2bc1d3ae8a2921e
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-23 02:08:15 +0000

    Few Changes
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-landing-page/tags/1.3@1166057 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Ultimate_Landing_Pages.php
+++ Ultimate_Landing_Pages.php
@@ -7,16 +7,17 @@ Plugin URI: http://web-settler.com/ultimate-landing-page/
 Author URI: http://web-settler.com/contact/
 Version: 1.3
 Donate link: http://web-settler.com/ultimate-landing-page/ 
+*/
 
-*******
+/********
 * Created By : Umar Bajwa
 * Copyright: 2014 http://web-settler.com/
 * Note : License Free to use, Free to modify, Permission required to redistribute.
 * Contact : http://web-settler.com/contact/
 * Support: http://web-settler.com/ulp-support/
 * Email : umar@web-setter.com
-*******
-*/
+********/
+
 include 'lpp_settings.php';
 include 'lpp_cs_post_type.php';
 include 'lpp_scripts.php';
--- lpp_seo.php
+++ lpp_seo.php
@@ -77,7 +77,6 @@ function lpp_f_seo_set($post){
 		<textarea type='text' style='height:200px; width:350px; float:left;' name='lpp_seo_meta_description' placeholder='Meta Description :' value='<?php echo $lpp_seo_meta_description; ?>'><?php echo $lpp_seo_meta_description; ?></textarea>
 		<br>
 
-        <input type='hidden' name='lpp_seo_keywords' placeholder='SEO Keywords seperated by commas:' value='<?php echo $lpp_seo_keywords; ?>'>
 
 	</div>
 
