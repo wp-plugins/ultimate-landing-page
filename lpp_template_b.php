53de59a9c9cfe7c8b573744e73d12f3c1ab99887
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-07 04:00:03 +0000

    Few Changes
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-landing-page/tags/1.3@1176057 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lpp_template_b.php
+++ lpp_template_b.php
@@ -255,9 +255,6 @@ body{
         <div id="lpp_benefit_3">
          <ul id="lpp_feature" class="lpp_p">
             <li><?php echo  get_post_meta( $post->ID , 'lpp_feature_1' , true ); ?></li>
-             <li><?php echo  get_post_meta( $post->ID , 'lpp_feature_2' , true ); ?></li>
-              <li><?php echo  get_post_meta( $post->ID , 'lpp_feature_3' , true ); ?></li>
-               <li><?php echo  get_post_meta( $post->ID , 'lpp_feature_4' , true ); ?></li>
 
             </ul>
 
