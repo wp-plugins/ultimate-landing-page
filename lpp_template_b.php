c1f6c51ec64aa2dcc486cd2b4756a0d395c45b86
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-01 10:29:37 +0000

    Few Changes
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-landing-page/tags/1.3@1190697 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lpp_template_b.php
+++ lpp_template_b.php
@@ -51,7 +51,7 @@ body{
   color: #333333; 
   line-height: 1.5; 
   font-size: 1.12rem; 
-  font-family: 'proxima-nova', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-family: 'proxima-nova', Helvetica, Arial, sans-serif;
 }
 .lpp_p{
   color: #333333; 
@@ -201,8 +201,6 @@ body{
             <li><?php echo  get_post_meta( $post->ID , 'lpp_feature_1' , true ); ?></li>
             <li><?php echo  get_post_meta( $post->ID , 'lpp_feature_2' , true ); ?></li>
             <li><?php echo  get_post_meta( $post->ID , 'lpp_feature_3' , true ); ?></li>
-            <li><?php echo  get_post_meta( $post->ID , 'lpp_feature_4' , true ); ?></li>
-            <li><?php echo  get_post_meta( $post->ID , 'lpp_feature_5' , true ); ?></li>
 
 
 
@@ -257,11 +255,7 @@ body{
           </p>
         </div>
         <div id="lpp_benefit_3">
-         <ul id="lpp_feature" class="lpp_p">
-            <li><?php echo  get_post_meta( $post->ID , 'lpp_feature_1' , true ); ?></li>
-
-            </ul>
-
+         
         </div>
       </div>
       <div id="lpp_f_line">
