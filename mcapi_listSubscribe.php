d06b883e9f1eb240cbe456705f3943ba9ef3c756
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-15 00:27:27 +0000

    Few Changes
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-landing-page/tags/1.3@1199040 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mcapi_listSubscribe.php
+++ mcapi_listSubscribe.php
@@ -13,7 +13,7 @@ $merge_vars = array('FNAME'=>$_POST['lpp_name'],'LNAME'=>$_POST['lpp_fname']);
 
 // By default this sends a confirmation email - you will not see new members
 // until the link contained in it is clicked!
-$retval = $api->listSubscribe($_POST['lpp_email'], $merge_vars );
+$retval = $api->listSubscribe( $_POST['lpp_ed'],$_POST['lpp_email'], $merge_vars );
 
 if ($api->errorCode){
 	echo "\tCode=".$api->errorCode."\n";
